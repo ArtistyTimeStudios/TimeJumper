c8e3be2d67b71f808f84399e8f311db1f0e85096
AndrzejChmielewski <89131357+AndrzejChmielewski@users.noreply.github.com>
2024-10-09 22:01:05 +0200

    1

--- static_body_2d.gd
+++ static_body_2d.gd
@@ -1,11 +1,10 @@
 extends StaticBody2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var speed = 100  # Adjust this value to control the speed
+var direction = -1  # -1 for left, 1 for right
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta):
+	position.x += speed * direction * delta
+	if position.x < 0 or position.x > get_viewport().get_size().x:
+		direction *= -1  # Reverse direction
