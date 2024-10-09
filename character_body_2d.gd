babf3bdcc64f8bf2fd0e0bafafa27ca455df599f
AndrzejChmielewski <89131357+AndrzejChmielewski@users.noreply.github.com>
2024-10-09 21:10:13 +0200

    2

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,23 +1,21 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -600.0
-
+const LEFT_MOUSE_BUTTON = 1  # Left mouse button is represented by the integer value 1
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# Handle jump with screen touch.
+	if (Input.is_action_just_pressed("ui_accept") or Input.is_mouse_button_pressed(LEFT_MOUSE_BUTTON)) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction != 0:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
