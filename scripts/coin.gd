cbb84aa2bfc2dd93f8cfdb5df31bedbeae49f918
Dominik Macinski <macinski.do@gmail.com>
2024-10-10 15:44:38 +0200

    12

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,12 +1,18 @@
 extends Area2D
 
+var score = 0
+
 @onready var game_manager = %GameManager
 @onready var animation_player = $AnimationPlayer
+@onready var coin_counter: Label = $"../CoinCOUNTER"
 
 func _on_body_entered(body):
-	print("+1 coin!")
+	game_manager.add_point()
 	queue_free()
 
+func add_point():
+	score += 1
+	score_label.text = "Score" + str(score) + " coins."
 
 
 #func _on_body_entered(body):
