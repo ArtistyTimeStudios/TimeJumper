34282b5517d91fdd2c0f09db3b885dafeb74b73d
Dominik Macinski <macinski.do@gmail.com>
2024-10-11 19:14:17 +0200

    coin and enemy

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -7,16 +7,27 @@ const Enemy_1 = preload("res://scenes/enemy.tscn")
 # The distance at which enemies will be spawned ahead of the camera
 var spawn_distance: float = 500.0
 var enemies_spawned: Array = []
+var enemy_speed: float = 100.0 # Speed at which the enemies will move to the left
+var min_spacing: float = 200.0 # Minimum distance between enemies
+var max_spacing: float = 600.0 # Maximum distance between enemies
+var min_spawn_time: float = 1.0 # Minimum wait time between spawns (in seconds)
+var max_spawn_time: float = 3.0 # Maximum wait time between spawns (in seconds)
 
 func _ready():
+	# Seed the random number generator
+	randomize()
+	
 	spawn_timer.connect("timeout", Callable(self, "_on_spawn_timer_timeout"))
-	spawn_timer.wait_time = 1.0 # Set the wait time to control spawning frequency
+	spawn_timer.wait_time = 1.0 # Initial spawn time, but will randomize after first spawn
 	spawn_timer.start()
 
 # This function is called whenever the spawn timer times out
 func _on_spawn_timer_timeout():
 	var camera_position = camera_2d.global_position
-	var spawn_position = Vector2(camera_position.x + spawn_distance, camera_position.y)
+	
+	# Generate random spacing between enemies
+	var random_spacing = randi_range(min_spacing, max_spacing)
+	var spawn_position = Vector2(camera_position.x + spawn_distance + random_spacing, camera_position.y)
 	
 	# Create an instance of the enemy scene and add it to the current scene
 	var new_enemy = Enemy_1.instantiate()
@@ -25,11 +36,19 @@ func _on_spawn_timer_timeout():
 	
 	# Keep track of enemies spawned
 	enemies_spawned.append(new_enemy)
+	
+	# Randomize the spawn timer for the next enemy
+	spawn_timer.wait_time = randf_range(min_spawn_time, max_spawn_time)
+	spawn_timer.start() # Restart the timer to apply the new wait time
 
-# Optionally remove enemies if they get too far behind the camera
+# This function is called every frame to update the game state
 func _process(_delta):
 	var camera_position = camera_2d.global_position
 	for enemy in enemies_spawned:
+		# Move the enemy to the left
+		enemy.global_position.x -= enemy_speed * _delta
+		
+		# Optionally remove enemies if they get too far behind the camera
 		if enemy.global_position.x < camera_position.x - spawn_distance * 2:
 			enemies_spawned.erase(enemy)
 			enemy.queue_free()
